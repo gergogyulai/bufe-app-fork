341c6e9547dfeae8b5733a380f2cb9cab590d42f
Saul Goodman <113334790+gergogyulai@users.noreply.github.com>
2023-03-21 23:07:39 +0100

    updated api/zaras undorito megoldas de ez van

--- src/routes/api/zaras/+server.js
+++ src/routes/api/zaras/+server.js
@@ -1,32 +1,49 @@
+import { bufeClosed } from "$lib/stores/Closed.js";
+
 export function GET() {
-  	const now = new Date();
-  	const cutoff = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 3, 0);
- 	const diff = cutoff - now;
-  	let message, zarva;
-
-  	if (diff <= 0) {
-    	message = "closed";
-    	zarva = true;
-  	} else if (diff <= 10 * 60000) {
-    	message = "10minLeft";
-    	zarva = false;
-  	} else if (diff <= 20 * 60000) {
-    	message = "20minLeft";
-    	zarva = false;
-  	} else if (diff <= 30 * 60000) {
-    	message = "30minLeft";
-    	zarva = false;
-  	} else if (diff <= 60 * 60000) {
-    	message = "60minLeft";
-    	zarva = false;
-  	} else {
-    	message = "moreThanOne";
-    	zarva = false;
-  	}
-
-  	const body = JSON.stringify({ message, zarva });
+	const now = new Date();
+	const zaras = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 7, 0, 0);
+	const nyitas = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 14, 0, 0);
+	const zarasDifference = zaras - now;
+	const nyitasDifference = nyitas - now;
+	const dayOfTheWeek = now.getDay();
+	let message, zarva;
+  
+	if (dayOfTheWeek === 0 || dayOfTheWeek === 6) {
+	  	message = "weekend";
+	  	zarva = true;
+		bufeClosed.set(true);
+	} else if (zarasDifference <= 0) {
+	  	message = "closed";
+	  	zarva = true;
+		bufeClosed.set(true);
+	} else if (zarasDifference <= 10 * 60000) {
+	  	message = "10minLeft";
+	  	zarva = false;
+		bufeClosed.set(false)
+	} else if (zarasDifference <= 20 * 60000) {
+	  	message = "20minLeft";
+	  	zarva = false;
+		bufeClosed.set(false)
+	} else if (zarasDifference <= 30 * 60000) {
+	  	message = "30minLeft";
+	  	zarva = false;
+		bufeClosed.set(false)
+	} else if (zarasDifference <= 60 * 60000) {
+	  	message = "60minLeft";
+	  	zarva = false;
+		bufeClosed.set(false)
+	} else if (nyitasDifference <= now){
+	  	message = "nyitva";
+	  	zarva = false;
+		bufeClosed.set(false)
+	} else {
+		message = "error with time";
+	}
+  
+	const body = JSON.stringify({ message, zarva });
 	const headers = { "Content-Type": "application/json" };
-  	const response = new Response(body, { headers });
-
-  	return response;
-}
+	const response = new Response(body, { headers });
+	
+	return response;
+}  
