1e053a0fba7f392238d7e8ad61d35003ef7fb759
Saul Goodman <113334790+gergogyulai@users.noreply.github.com>
2023-03-29 02:09:40 +0200

    updated tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,11 +3,6 @@ module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {
-      colors: {
-        'coopurple' : '#480CA8',
-        'coopurple2' : '#7209B7',
-        
-      },
     },
   },
   plugins: [],
