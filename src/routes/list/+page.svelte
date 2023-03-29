8a5b5c9af4023f60cd7d73bba0a7b83b6315a3c0
Saul Goodman <113334790+gergogyulai@users.noreply.github.com>
2023-03-29 23:28:38 +0200

    better navbar

--- src/routes/list/+page.svelte
+++ src/routes/list/+page.svelte
@@ -62,7 +62,7 @@
 		}
 	}
 </script>
-<main class=" h-screen w-screen overflow-x-hidden overflow-y-scroll bg-slate-900 pb-20">
+<main class=" h-screen w-screen overflow-x-hidden overflow-y-scroll bg-gray-100 dark:bg-slate-900 pb-20">
    <Topbar
         target={'Menü'}
         targeturl={'/'}
@@ -76,10 +76,10 @@
     <div class="flex justify-center items-center my-5">
         <label class="group" for="searchBar">
             <div class="flex focus-within:ring-1 focus-within:ring-blue-600 rounded-lg">
-               <div class=" flex items-center justify-center rounded-l-lg border-none bg-gray-300">
+               <div class=" flex items-center justify-center rounded-l-lg border-none bg-gray-200">
                     <i class="group-focus-within:text-neutral-800 p-1 pl-2 text-neutral-500 fa-solid fa-magnifying-glass"></i>
                 </div>
-               <input placeholder="Keresés" id="searchBar" class="group-focus-within:placeholder-neutral-600 bg-gray-300 border-0 p-2 px-2 rounded-r-lg focus:outline-none w-20 group-focus-within:w-48 transition-[width] ease-in-out duration-150" type="text" bind:value={searchWord} on:input={search}>
+               <input placeholder="Keresés" id="searchBar" class="group-focus-within:placeholder-neutral-600 bg-gray-200 border-0 p-2 px-2 rounded-r-lg focus:outline-none w-20 group-focus-within:w-48 transition-[width] ease-in-out duration-150" type="text" bind:value={searchWord} on:input={search}>
             </div>
         </label>
     </div>
@@ -99,7 +99,7 @@
             <div class='flex flex-col w-full items-center'>
 		    	{#each data.termekek as termek}
 		    	{#if termek.kategoria == 'Étel'}
-                    <div class="flex flex-row mb-4 w-full items-center rounded-lg bg-neutral-300 dark:bg-slate-800 dark:text-neutral-100 text-neutral-900" class:opacity-40={termek.darab == 0}>
+                    <div class="flex flex-row mb-4 w-full items-center rounded-lg bg-gray-200 dark:bg-slate-800 dark:text-neutral-100 text-neutral-900" class:opacity-40={termek.darab == 0}>
                         <div class="mr-4">
                             <img class=" rounded-l-lg h-32 w-32 object-cover" src="favicon.png" alt="">
                         </div>
@@ -107,7 +107,7 @@
                             <div class="justify-self-end">
                                 <div class="flex flex-col">
                                     <div class=" h-fit">
-                                        <p class=" text-xl text-blue-300">{termek.termek}</p>
+                                        <p class=" text-xl dark:text-blue-300">{termek.termek}</p>
                                         <p class="text-md">Elérhető: {termek.darab}db</p>
                                     </div>
                                     <div class="flex items-center">
@@ -128,7 +128,7 @@
                 <div class='flex flex-col w-full items-center'>
                     {#each data.termekek as termek}
                     {#if termek.kategoria == 'Ital'}
-                        <div class="flex flex-row mb-4 w-full items-center rounded-lg bg-neutral-300 dark:bg-slate-800 dark:text-neutral-100 text-neutral-900" class:opacity-40={termek.darab == 0}>
+                        <div class="flex flex-row mb-4 w-full items-center rounded-lg bg-gray-200 dark:bg-slate-800 dark:text-neutral-100 text-neutral-900" class:opacity-40={termek.darab == 0}>
                             <div class="mr-4">
                                 <img class=" rounded-l-lg h-32 w-32 object-cover" src="favicon.png" alt="">
                             </div>
@@ -136,7 +136,7 @@
                                 <div class="justify-self-end">
                                     <div class="flex flex-col">
                                         <div class=" h-fit">
-                                            <p class=" text-xl text-blue-300">{termek.termek}</p>
+                                            <p class=" text-xl dark:text-blue-300">{termek.termek}</p>
                                             <p class="text-md">Elérhető: {termek.darab}db</p>
                                         </div>
                                         <div class="flex items-center">
@@ -157,15 +157,15 @@
                 <div class='flex flex-col w-full items-center'>
                     {#each data.termekek as termek}
                     {#if termek.kategoria == 'Nasi'}
-                        <div class="flex flex-row mb-4 w-full items-center rounded-lg bg-neutral-300 dark:bg-slate-800 dark:text-neutral-100 text-neutral-900" class:opacity-40={termek.darab == 0}>
+                        <div class="flex flex-row mb-4 w-full items-center rounded-lg bg-gray-200 dark:bg-slate-800 dark:text-neutral-100 text-neutral-900" class:opacity-40={termek.darab == 0}>
                             <div class="mr-4">
-                                <img class=" rounded-l-lg h-32 w-32 object-cover" src="favicon.png" alt="">
+                                <img class=" rounded-l-lg h-32 w-32 object-cover" src="hamburger.jpg" alt="">
                             </div>
                             <a href="{termek.termek}?referrer=/list">
                                 <div class="justify-self-end">
                                     <div class="flex flex-col">
                                         <div class=" h-fit">
-                                            <p class=" text-xl text-blue-300">{termek.termek}</p>
+                                            <p class=" text-xl dark:text-blue-300">{termek.termek}</p>
                                             <p class="text-md">Elérhető: {termek.darab}db</p>
                                         </div>
                                         <div class="flex items-center">
@@ -186,7 +186,7 @@
             <div class='flex flex-col w-full items-center'>
 		    	{#each data.termekek as termek}
 		    	{#if termek.kategoria == 'Egyéb'}
-                    <div class="flex flex-row mb-4 w-full items-center rounded-lg bg-neutral-300 dark:bg-slate-800 dark:text-neutral-100 text-neutral-900" class:opacity-40={termek.darab == 0}>
+                    <div class="flex flex-row mb-4 w-full items-center rounded-lg bg-gray-200 dark:bg-slate-800 dark:text-neutral-100 text-neutral-900" class:opacity-40={termek.darab == 0}>
                         <div class="mr-4">
                             <img class=" rounded-l-lg h-32 w-32 object-cover" src="favicon.png" alt="">
                         </div>
@@ -194,7 +194,7 @@
                             <div class="justify-self-end">
                                 <div class="flex flex-col">
                                     <div class=" h-fit">
-                                        <p class=" text-xl text-blue-300">{termek.termek}</p>
+                                        <p class=" text-xl dark:text-blue-300">{termek.termek}</p>
                                         <p class="text-md">Elérhető: {termek.darab}db</p>
                                     </div>
                                     <div class="flex items-center">
@@ -212,11 +212,15 @@
  	</Swiper>
 
     <div class=" fixed left-0 bottom-0 mb-7 flex flex-row items-center justify-center w-full z-50">
-        <div in:fly={{y: 100}} class='flex justify-center items-center bg-slate-700 text-white h12 rounded-xl py-2 px-3'>
-            <div class="Étel mr-2 bg-slate-600 p-1.5 rounded-lg" class:nav-active='{$navigation == 0}' on:click={() => {navigate(0)}}><span>Étel</span></div>
-            <div class="Ital mr-2 bg-slate-600 p-1.5 rounded-lg" class:nav-active='{$navigation == 1}' on:click={() => {navigate(1)}}><span>Ital</span></div>
-            <div class="Nasi mr-2 bg-slate-600 p-1.5 rounded-lg" class:nav-active='{$navigation == 2}' on:click={() => {navigate(2)}}><span>Nasi</span></div>
-            <div class="Egyéb bg-slate-600 p-1.5 rounded-lg" class:nav-active='{$navigation == 3}' on:click={() => {navigate(3)}}><span>Egyéb</span></div>
+        <div in:fly={{y: 100}} class='flex justify-center items-center backdrop-blur-lg dark:backdrop-blur-xl bg-gray-300 dark:bg-slate-700 bg-opacity-40 dark:bg-opacity-50 text-neutral-800 dark:text-white h12 rounded-xl py-2 px-3'>
+            <div class="Étel mr-2 p-1.5 rounded-lg" class:nav-active='{$navigation == 0}' on:click={() => {navigate(0)}}><span>Étel</span></div>
+            <div class="Ital mr-2 p-1.5 rounded-lg" class:nav-active='{$navigation == 1}' on:click={() => {navigate(1)}}><span>Ital</span></div>
+            <div class="Nasi mr-2 p-1.5 rounded-lg" class:nav-active='{$navigation == 2}' on:click={() => {navigate(2)}}><span>Nasi</span></div>
+            <div class="Egyéb p-1.5 rounded-lg" class:nav-active='{$navigation == 3}' on:click={() => {navigate(3)}}><span>Egyéb</span></div>
         </div>
+    </div>
+
+    <div class="fixed left-0 -bottom-10 h-20 blur-xl w-screen">
+
     </div>
 </main>
