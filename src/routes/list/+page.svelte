12e3d6e702fe1d46f6a0fbad27ab2d675c703841
Saul Goodman <113334790+gergogyulai@users.noreply.github.com>
2023-03-29 22:46:39 +0200

    list redesign + egyeb kategoria hozzadva

--- src/routes/list/+page.svelte
+++ src/routes/list/+page.svelte
@@ -9,7 +9,6 @@
 
    export let data;
 
-   const darab = data.termekek.darab
    let swiper;
    let cartshow = 0;
 
@@ -38,7 +37,8 @@
 			let kategoria = {
 				'Étel': 0,
 				'Ital': 0,
-				'Nasi': 0
+				'Nasi': 0,
+            'Egyéb':0
 			}
 
 			data.termekek.forEach(termek => {
@@ -54,28 +54,34 @@
 					break;
 				case kategoria.Nasi:
 					navigate(2)
+               break;
+            case kategoria.Egyéb:
+					navigate(3)
+               break;
 			}
 		}
-		
 	}
 </script>
-
-<main>
+<main class=" h-screen w-screen overflow-x-hidden overflow-y-scroll bg-slate-900 pb-20">
 
    <Topbar
       target={'Menü'}
       targeturl={'/'}
+      background={'none'}
       text={'Termékek'}
-      background={'#252525'}
       flyin={{y: -200}}
       hideProfile={0}
+      kosar={true}
    ></Topbar>
 
-	<div class="flex justify-center mt-3">
-		<div class="flex items-center m-1">
-			<input placeholder="Kezdj el gépelni" class=" bg-gray-300 border-0 w-full rounded-lg p-1 px-2 outline" type="text" bind:value={searchWord} on:input={search}>
-		</div>
-	</div>
+      <div class="flex justify-center items-center my-5">
+         <label class="group" for="searchBar">
+            <div class="flex focus-within:ring-1 focus-within:ring-blue-600 rounded-lg">
+               <div class=" flex items-center justify-center rounded-l-lg border-none bg-gray-300"><i class="group-focus-within:text-neutral-800 p-1 pl-2 text-neutral-500 fa-solid fa-magnifying-glass"></i></div>
+               <input placeholder="Keresés" id="searchBar" class="group-focus-within:placeholder-neutral-600 bg-gray-300 border-0 p-2 px-2 rounded-r-lg focus:outline-none w-20 group-focus-within:w-48 transition-[width] ease-in-out duration-150" type="text" bind:value={searchWord} on:input={search}>
+            </div>
+         </label>
+      </div>
 
 	<Swiper
 	initialSlide={$navigation}
@@ -86,16 +92,30 @@
 	on:slideChange={e => {$navigation = e.detail[0].activeIndex}}
 	on:swiper={e => {swiper = e.detail[0]}}
  	>
+   
 		<SwiperSlide>
-			<div in:slide={{duration: 800}} class='list-grid'>
-				<div class='grid-container'>
+			<div in:slide={{duration: 800}} class='px-8'>
+            <div class='flex flex-col w-full items-center'>
 					{#each data.termekek as termek}
 						{#if termek.kategoria == 'Étel'}
-							<div class="inner-grid" class:opacity-40={termek.darab == 0}>
-								<a data-sveltekit-noscroll href="{termek.termek}?referrer=/list" class='grid-cell'><img src='favicon.png' alt=''></a>
-								<a data-sveltekit-noscroll href="{termek.termek}?referrer=/list" class='grid-cell'>{termek.termek}</a>
-								<a data-sveltekit-noscroll href="{termek.termek}?referrer=/list" class='grid-cell'>{termek.ar} Ft</a>
-							</div>
+                     <div class="flex flex-row mb-4 w-full items-center rounded-lg bg-neutral-300 dark:bg-slate-800 dark:text-neutral-100 text-neutral-900" class:opacity-40={termek.darab == 0}>
+                        <div class="mr-4">
+                           <img class=" rounded-l-lg h-32 w-32 object-cover" src="favicon.png" alt="">
+                        </div>
+                        <a href="{termek.termek}?referrer=/list">
+                           <div class="justify-self-end">
+                              <div class="flex flex-col">
+                                 <div class=" h-fit">
+                                    <p class=" text-xl text-blue-300">{termek.termek}</p>
+                                    <p class="text-md">Elérhető: {termek.darab}db</p>
+                                 </div>
+                                 <div class="flex items-center">
+                                    <p class="text-2xl">{termek.ar} Ft</p>
+                                 </div>
+                              </div>
+                           </div>
+                        </a>
+                  </div>
 						{/if}
 					{/each}
 				</div>
@@ -103,15 +123,28 @@
 		</SwiperSlide>
 
 		<SwiperSlide>
-			<div in:slide={{duration: 800}} class='list-grid'>
-				<div class='grid-container'>
+			<div in:slide={{duration: 800}} class='px-8'>
+            <div class='flex flex-col w-full items-center'>
 					{#each data.termekek as termek}
 						{#if termek.kategoria == 'Ital'}
-							<div class="inner-grid" class:opacity-50={termek.darab == 0}>
-								<a data-sveltekit-noscroll href="{termek.termek}?referrer=/list" class='grid-cell'><img src='favicon.png' alt=''></a>
-								<a data-sveltekit-noscroll href="{termek.termek}?referrer=/list" class='grid-cell'>{termek.termek}</a>
-								<a data-sveltekit-noscroll href="{termek.termek}?referrer=/list" class='grid-cell'>{termek.ar} Ft</a>
-							</div>
+                     <div class="flex flex-row mb-4 w-full items-center rounded-lg bg-neutral-300 dark:bg-slate-800 dark:text-neutral-100 text-neutral-900" class:opacity-40={termek.darab == 0}>
+                        <div class="mr-4">
+                           <img class=" rounded-l-lg h-32 w-32 object-cover" src="favicon.png" alt="">
+                        </div>
+                        <a href="{termek.termek}?referrer=/list">
+                           <div class="justify-self-end">
+                              <div class="flex flex-col">
+                                 <div class=" h-fit">
+                                    <p class=" text-xl text-blue-300">{termek.termek}</p>
+                                    <p class="text-md">Elérhető: {termek.darab}db</p>
+                                 </div>
+                                 <div class="flex items-center">
+                                    <p class="text-2xl">{termek.ar} Ft</p>
+                                 </div>
+                              </div>
+                           </div>
+                        </a>
+                  </div>
 						{/if}
 					{/each}
 				</div>
@@ -119,169 +152,80 @@
 		</SwiperSlide>
 
 		<SwiperSlide>
-			<div in:slide={{duration: 800}} class='list-grid'>
-				<div class='grid-container'>
+			<div in:slide={{duration: 800}} class='px-8'>
+            <div class='flex flex-col w-full items-center'>
 					{#each data.termekek as termek}
 						{#if termek.kategoria == 'Nasi'}
-							<div class="inner-grid" class:opacity-50={termek.darab == 0}>
-								<a data-sveltekit-noscroll href="{termek.termek}?referrer=/list" class='grid-cell'><img src='favicon.png' alt=''></a>
-								<a data-sveltekit-noscroll href="{termek.termek}?referrer=/list" class='grid-cell'>{termek.termek}</a>
-								<a data-sveltekit-noscroll href="{termek.termek}?referrer=/list" class='grid-cell'>{termek.ar} Ft</a>
-							</div>
+                     <div class="flex flex-row mb-4 w-full items-center rounded-lg bg-neutral-300 dark:bg-slate-800 dark:text-neutral-100 text-neutral-900" class:opacity-40={termek.darab == 0}>
+                        <div class="mr-4">
+                           <img class=" rounded-l-lg h-32 w-32 object-cover" src="favicon.png" alt="">
+                        </div>
+                        <a href="{termek.termek}?referrer=/list">
+                           <div class="justify-self-end">
+                              <div class="flex flex-col">
+                                 <div class=" h-fit">
+                                    <p class=" text-xl text-blue-300">{termek.termek}</p>
+                                    <p class="text-md">Elérhető: {termek.darab}db</p>
+                                 </div>
+                                 <div class="flex items-center">
+                                    <p class="text-2xl">{termek.ar} Ft</p>
+                                 </div>
+                              </div>
+                           </div>
+                        </a>
+                  </div>
 						{/if}
 					{/each}
 				</div>
 			</div>
 		</SwiperSlide>
+
+      <SwiperSlide>
+			<div in:slide={{duration: 800}} class='px-8'>
+            <div class='flex flex-col w-full items-center'>
+					{#each data.termekek as termek}
+						{#if termek.kategoria == 'Egyéb'}
+                     <div class="flex flex-row mb-4 w-full items-center rounded-lg bg-neutral-300 dark:bg-slate-800 dark:text-neutral-100 text-neutral-900" class:opacity-40={termek.darab == 0}>
+                        <div class="mr-4">
+                           <img class=" rounded-l-lg h-32 w-32 object-cover" src="favicon.png" alt="">
+                        </div>
+                        <a href="{termek.termek}?referrer=/list">
+                           <div class="justify-self-end">
+                              <div class="flex flex-col">
+                                 <div class=" h-fit">
+                                    <p class=" text-xl text-blue-300">{termek.termek}</p>
+                                    <p class="text-md">Elérhető: {termek.darab}db</p>
+                                 </div>
+                                 <div class="flex items-center">
+                                    <p class="text-2xl">{termek.ar} Ft</p>
+                                 </div>
+                              </div>
+                           </div>
+                        </a>
+                  </div>
+						{/if}
+					{/each}
+				</div>
+			</div>
+		</SwiperSlide>
+
  	</Swiper>
 
- {#if cartshow}
- <div in:fly={{y: 100}} style='margin-bottom: 6.5vh;' class='nav'>
-	 <div class='Étel' on:click={() => {navigate(0)}}><p class:active='{$navigation == 0}'>Étel</p></div>
-	 <div class='Ital' on:click={() => {navigate(1)}}><p class:active='{$navigation == 1}'>Ital</p></div>
-	 <div class='Nasi' on:click={() => {navigate(2)}}><p class:active='{$navigation == 2}'>Nasi</p></div>
- </div>
- 
- <a data-sveltekit-noscroll href="kosar?referrer=/list">
-	 <div in:fly={{y: 100, delay: 100}} class='cart'>
-		 <div class="cart-grid">
-			 <div class="cart-cell">
-				 <img src="shopping-basket.png" alt=""> <b>{$total.ar} Ft</b><p>({$total.darab} db termék a kosárban.)</p>
-			 </div>
-		 </div>
-	 </div>
- </a>
- 
- {:else}
- <div in:fly={{y: 200}} class='nav'>
-	 <div class='Étel' on:click={() => {navigate(0)}}><p class:active='{$navigation == 0}'>Étel</p></div>
-	 <div class='Ital' on:click={() => {navigate(1)}}><p class:active='{$navigation == 1}'>Ital</p></div>
-	 <div class='Nasi' on:click={() => {navigate(2)}}><p class:active='{$navigation == 2}'>Nasi</p></div>
- </div>
- {/if}
+    <div class=" fixed left-0 bottom-0 mb-7 flex flex-row items-center justify-center w-full z-50">
+         <div in:fly={{y: 100}} class='flex justify-center items-center bg-slate-700 text-white h12 rounded-xl py-2 px-3'>
+            <div class="Étel mr-2 bg-slate-600 p-1.5 rounded-lg" class:nav-active='{$navigation == 0}' on:click={() => {navigate(0)}}><span>Étel</span></div>
+            <div class="Ital mr-2 bg-slate-600 p-1.5 rounded-lg" class:nav-active='{$navigation == 1}' on:click={() => {navigate(1)}}><span>Ital</span></div>
+            <div class="Nasi mr-2 bg-slate-600 p-1.5 rounded-lg" class:nav-active='{$navigation == 2}' on:click={() => {navigate(2)}}><span>Nasi</span></div>
+            <div class="Egyéb bg-slate-600 p-1.5 rounded-lg" class:nav-active='{$navigation == 3}' on:click={() => {navigate(3)}}><span>Egyéb</span></div>
+
+         </div>
+    </div>
+
+
+   <!-- <div in:fly={{y: 200}} class='nav'>
+	   <div class='Étel' on:click={() => {navigate(0)}}><p class:active='{$navigation == 0}'>Étel</p></div>
+	   <div class='Ital' on:click={() => {navigate(1)}}><p class:active='{$navigation == 1}'>Ital</p></div>
+	   <div class='Nasi' on:click={() => {navigate(2)}}><p class:active='{$navigation == 2}'>Nasi</p></div>
+   </div> -->
 
 </main>
-
-<style lang='scss'>
-
-main {
-      width: 100%;
-      height: 100%;
-      overflow-y: scroll;
-      position: fixed;
-
-      .list-grid {
-         display: grid;
-
-         .grid-container {
-            margin: 3%;
-            margin-bottom: 40%;
-            display: grid;
-            row-gap: .6vh;
-
-               .inner-grid {
-                  display: grid;
-                  grid-template-columns: 25% 25% 25% 25%;
-                  background-color: #252525;
-                  border-radius: 1em;
-
-                  &:nth-of-type(2n) {
-                     background-color: #161616;
-                  }
-
-                  .grid-cell {
-                     display: flex;
-                     justify-content: center;
-                     align-items: center;
-                     color: rgba(255, 255, 255, 0.9);
-                     height: 90px;
-
-                     &:nth-of-type(2n) {  // termek neve cell
-                        grid-column-start: 2;
-                        grid-column-end: 4;
-                     }
-
-                     img {
-                        height: 100%;
-                     }
-                  }
-               }
-         }
-
-      }
-
-      .nav {
-         display: flex;
-         background-color: #252525ea;
-         color: rgba(255, 255, 255, 0.877);
-         height: 12vh;
-         width: 100vw;
-         position: fixed;
-         bottom: 0;
-         left: 0;
-         z-index: 20;
-         margin-bottom: 3vh;
-         border-radius: 3em;
-         transform: scale(60%);
-         font-weight: bold;
-         border: 1px solid rgba(255, 255, 255, 0.048);
-
-         div {
-            display: flex;
-            flex: 33.3%;
-            justify-content: center;
-            align-items: center;
-         }
-
-         .active {
-            padding: 2vh;
-            background-color: var(--accent-color);
-            border-radius: 20px;
-            color: black;
-            transition: all .25s ease-in-out;
-         }
-      }
-      
-      .cart {
-         position: fixed;
-         bottom: 0;
-         left: 0;
-         height: 3.5em;
-         background-color: var(--accent-color);
-         border-top-left-radius: 2.8em;
-         border-top-right-radius: 2.8em;
-         width: calc(98% - 2%); // * NAAAAGYON HACKY, de nem megy mashogy. Utálom a css-t
-         margin-left: 2%;
-         padding: .5ch 0;
-			z-index: 20;
-
-         .cart-grid {
-            height: 100%;
-            width: 100%;
-            display: grid;
-
-            .cart-cell {
-               display: flex;
-               align-items: center;
-               justify-content: center;
-               color: black;
-
-               img {
-                  width: 12%;
-               }
-
-               b {
-                  font-size: large;
-                  margin-right: 3%;
-                  margin-left: 1%;
-                  margin-top: 1%;
-               }
-
-               p {
-                  margin-top: 1%;
-               }
-            }
-         }
-      }
-	}
- </style>
