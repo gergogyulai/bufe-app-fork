fbb07e0f056fb812368ba439564728105aa065c4
Saul Goodman <113334790+gergogyulai@users.noreply.github.com>
2023-04-02 21:32:25 +0200

    Tailwind update to v3.3.1

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {
+      boxShadow: {
+        'highlight': 'inset 0 1px 0 0 #ffffff0d',
+      }
     },
   },
   plugins: [],
