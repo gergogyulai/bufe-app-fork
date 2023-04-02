06299570840e230e1ff0d0a01585124a09ee1460
Saul Goodman <113334790+gergogyulai@users.noreply.github.com>
2023-04-02 21:55:10 +0200

    mukodo atvetel, fizetesi mod valaszto, radio button lecsereles

--- src/routes/megrendeles/+page.server.js
+++ src/routes/megrendeles/+page.server.js
@@ -1,17 +1,46 @@
 import { fail } from '@sveltejs/kit';
 
 export const ssr = false;
+let atveteliOpciok = [];
 
 export async function load({ locals }) {
+	const now = new Date();
+
+	const elsoSzunet = new Date().setHours(8, 45, 0, 0);
+	const masodikSzunet = new Date().setHours(9, 40, 0, 0);
+	const harmadikSzunet = new Date().setHours(10, 35, 0, 0);
+	const negyedikSzunet = new Date().setHours(11, 30, 0, 0);
+	const ebedSzunet = new Date().setHours(12, 25, 0, 0);
+	const hatodikSzunet = new Date().setHours(13, 30, 0, 0);
+
+	if (now < elsoSzunet) {
+		atveteliOpciok = [{value: 'Most', label: 'Most'},{value: '2.szunet', label: '2. szünet (9:40-9:50)'},{value: '3.szunet', label: '3. szünet (10:35-10:45)'},{value: '4.szunet', label: '4. szünet (11:30-11:40)'},{value: '5.szunet', label: 'Ebédszünet (12:25-12:45)'},{value: '6.szunet', label: '6. szünet (13:30-13:35)'}];
+	} else if (now < masodikSzunet) {
+		atveteliOpciok = [{value: 'Most', label: 'Most'},{value: '3.szunet', label: '3. szünet (10:35-10:45)'},{value: '4.szunet', label: '4. szünet (11:30-11:40)'},{value: '5.szunet', label: 'Ebédszünet (12:25-12:45)'},{value: '6.szunet', label: '6. szünet (13:30-13:35)'}];
+	} else if (now < harmadikSzunet) {
+		atveteliOpciok = [{value: 'Most', label: 'Most'},{value: '4.szunet', label: '4. szünet (11:30-11:40)'},{value: '5.szunet', label: 'Ebédszünet (12:25-12:45)'},{value: '6.szunet', label: '6. szünet (13:30-13:35)'}];
+	} else if (now < negyedikSzunet) {
+		atveteliOpciok = [{value: 'Most', label: 'Most'},{value: '5.szunet', label: 'Ebédszünet (12:25-12:45)'},{value: '6.szunet', label: '6. szünet (13:30-13:35)'}];
+	} else if (now < ebedSzunet) {
+		atveteliOpciok = [{value: 'Most', label: 'Most'},{value: '6.szunet', label: '6. szünet (13:30-13:35)'}];
+	} else if (now < hatodikSzunet) {
+		atveteliOpciok = [{value: 'Most', label: 'Most'}];
+	} else {
+		atveteliOpciok = [{value: 'Most', label: 'Most'}];
+	}
+
 	return {
-		'termekek': structuredClone(await locals.pb.collection('termekek').getFullList(1, {}))
+		'termekek': structuredClone(await locals.pb.collection('termekek').getFullList(1, {})), 
+		atveteliOpciok
 	};
-}
+};
 
 export const actions = {
 	default: async ({ request, locals }) => {
 		const data = Object.fromEntries(await request.formData());
 		const rendeles = JSON.parse(data.rendeles);
+		const paymentMethod = data.paymentMethod;
+		const atveteliIdo = data.atveteliIdo;
 		let total = 0;
 
 		for (const termek in rendeles) {
@@ -30,6 +59,6 @@ export const actions = {
 				return fail(409, { 'error': `Túl sok ${record.termek} a kosárban!`, 'sok': record.termek });
 		}
 
-		await locals.pb.collection('rendelesek').create({ 'rendelo': locals.pb.authStore.baseModel.id, 'termekek': rendeles, total, 'status': 'fuggoben', 'name': locals.pb.authStore.baseModel.name });
+		await locals.pb.collection('rendelesek').create({ 'rendelo': locals.pb.authStore.baseModel.id, 'termekek': rendeles, total, 'status': 'fuggoben', 'name': locals.pb.authStore.baseModel.name, paymentMethod, atveteliIdo });
 	}
 };
--- src/routes/megrendeles/+page.svelte
+++ src/routes/megrendeles/+page.svelte
@@ -5,17 +5,22 @@
 	import { deserialize } from '$app/forms';
 	import { cart, total } from "$lib/stores/Cart.js";
 	import Topbar from '$lib/components/Topbar.svelte';
-	import toast, {Toaster} from 'svelte-french-toast';
+	import { bufeClosed } from "$lib/stores/BufeClosed.js";
+  	import { each } from "svelte/internal";
 
- 
+	let bufeClosedStatus = $bufeClosed;
+
 	export let data;
 	let confirmShow = false;
- 
+	let paymentMethod = "Készpénz";
+ 	let atveteliIdo = 'Válassz egy átvételi időt';
+
+
 	if (localStorage.getItem('CartContent') != null) {
 	   $cart = JSON.parse(localStorage.getItem('CartContent'));
 	   $total = JSON.parse(localStorage.getItem('Total'));
 	}
- 
+
 	function recalculate() {
 	   $total = { 'ar': 0, 'darab': 0 };
 		 Object.keys($cart).forEach(termek => {
@@ -66,29 +71,36 @@
 	   };
 	};
  
-	async function handleSubmit(event) {
+	async function handleSubmit() {
+		if (atveteliIdo === ''){
+			alert("Nem adtál meg átvételi idot");
+			confirmShow = false;
+		} else if (atveteliIdo === 'Válassz egy átvételi időt'){
+			alert("Nem adtál meg átvételi idot");
+			confirmShow = false;
+		}
+
 	   const data = new FormData(this);
 	   const response = await fetch(this.action, {
 		  method: 'POST',
 		  body: data
-	   })
+	   });
  
 	   const result = deserialize(await response.text());
  
 	   if (result.type == 'success') {
 		  urites();
-		  goto('/rendelesek')
+		  goto('/rendelesek');
 	   } else {
 			 alert(result.data.error);
 			 delete $cart[result.data.sok];
 			 $cart = $cart; // kell reactivity miatt
 			 recalculate();
-		 }
-	}
- 
- </script>
+		 };
+	};
+</script>
 
-<main class="h-screen w-screen overflow-x-hidden overflow-auto text-neutral-900 bg-white dark:text-white dark:bg-neutral-900">
+<main class="h-screen w-screen overflow-x-hidden overflow-auto text-neutral-900 bg-white dark:text-white dark:bg-slate-900">
 
 	<Topbar
 	target={'Vissza'}
@@ -100,17 +112,20 @@
 	></Topbar>
 
 	{#if confirmShow}
-	<div class="h-screen w-screen fixed top-0 left-0 bottom-0 right-0 bg-black bg-opacity-60 backdrop-blur-sm flex justify-center items-center">
+	<div class="h-screen w-screen fixed top-0 left-0 bottom-0 right-0 bg-black bg-opacity-60 backdrop-blur-sm flex justify-center items-center z-50">
 		<div class="flex justify-center items-center flex-col w-3/4 lg:w-2/6 bg-gray-100 dark:bg-neutral-800 rounded-lg p-4 text-black dark:text-white drop-shadow-xl select-none">
 			<div class="flex w-full justify-center text-5xl m-2 drop-shadow-lg">
 				<span class="fa-regular fa-circle-exclamation"></span>
 			</div>
 			<span class="m-2 text-lg font-bold text-center drop-shadow-lg">Biztosan leadod rendelésed?</span>
+			<span class="m-0.5 text-center drop-shadow-lg">Átvétel: {atveteliIdo}</span>
 			<p class="text-center text-red-500 text-sm font-bold my-2 drop-shadow-lg ">Rendelésedet köteles vagy átvenni, 3 nem átvett rendelés után kizárunk az app használatából!</p>
 			<div class="flex w-full my-4 justify-center drop-shadow-lg">
 				<button class=" dark:bg-neutral-700 dark:hover:bg-neutral-600 hover:bg-gray-300 bg-gray-300 p-2 px-3 mr-2 rounded-lg" on:click={() => confirmShow = false}>Mégsem</button>
 				<form method="POST" on:submit|preventDefault={handleSubmit}>
 					<input hidden type="text" name="rendeles" value="{JSON.stringify($cart)}">
+					<input hidden type="text" name="atveteliIdo" value="{atveteliIdo}">
+					<input hidden type="text" name="paymentMethod" value="{paymentMethod}">
 					<button class=" hover:bg-green-400 bg-green-500 p-2 px-3 rounded-lg font-bold text-white">Igen</button>
 				</form>
 			</div>
@@ -121,9 +136,9 @@
 	<div class="px-8">
 		<h2 class="text-center text-3xl m-5">Rendelésed tartalma</h2>
 
-		<div class=" bg-neutral-800 rounded-lg border border-gray-200 dark:border-neutral-700 mb-4">
+		<div class=" bg-gray-100 dark:bg-slate-800 rounded-lg border border-gray-200 dark:border-slate-700 mb-4">
 			{#each Object.keys($cart) as termek, i (i)}
-			<div href="#" class=" h-24 p-1 flex flex-row items-center justify-between bg-white rounded-lg shadow dark:bg-neutral-800">
+			<div href="#" class=" h-24 p-1 flex flex-row items-center justify-between bg-gray-100 dark:bg-slate-800">
 				<img class="object-cover h-full" src="favicon.png" alt="">
 				<span class="text-xl">{termek}</span>
 				<div class="mr-2">
@@ -139,7 +154,7 @@
 		</div>
 
 		{#if Object.keys($cart).length != 0}
-		<div class="flex flex-col justify-center items-center w-full p-2 bg-neutral-100 dark:bg-neutral-800 border border-gray-200 dark:border-neutral-700 rounded-lg mb-6 ">
+		<div class="flex flex-col justify-center items-center w-full p-2 bg-neutral-100 dark:bg-slate-800 border border-gray-200 dark:border-slate-700 rounded-lg mb-6 ">
 			
 			{#key $total}
 			<div class="flex flex-col justify-center my-2">
@@ -148,45 +163,47 @@
 			</div>
 			{/key}
 
-			<div class="mb-2 w-full flex flex-col justify-center items-center">
+			<div class="mb-2 w-full flex flex-col justify-center items-center" >
 				<p class=" text-center mt-2">Fizetési mód:</p>
-				<div class=" flex items-center justify-center text-sm font-medium mb-4">
-					<div class="flex items-center mr-2">
-						<input checked id="radio-0" type="radio" value="" name="default-radio" class="w-4 h-4 text-cyan-600 bg-gray-100 border-gray-300 focus:ring-0 dark:ring-offset-gray-800 dark:bg-gray-700 dark:border-gray-600">
-						<label for="radio-0" class="ml-2 text-black dark:text-white">Készpénz</label>
+				<div class=" flex items-center justify-center mb-4">
+					<div class="flex items-center justify-center bg-gray-200 dark:bg-slate-600 p-2 cursor-pointer rounded-lg mr-2" class:selectedpayment={paymentMethod == "Készpénz"} on:click={() => {paymentMethod = 'Készpénz'; console.log(paymentMethod);}}>
+						<span>Készpénz</span>
 					</div>
 					{#if $total.ar < 500}
-						<div class="flex items-center ">
-							<input disabled id="radio-1" type="radio" value="" name="default-radio" class="w-4 h-4 text-cyan-600 bg-gray-100 border-gray-300 dark:ring-offset-gray-800 focus:ring-0 dark:bg-gray-700 dark:border-gray-600">
-							<label for="radio-1" class="ml-2 text-black dark:text-white">Bankkártya (min. 500Ft)</label>
-						</div>
+					<div class="flex items-center justify-center bg-gray-200 dark:bg-slate-600 p-2 cursor-not-allowed rounded-lg opacity-60">
+						<span>Bankkártya</span>
+					</div>
 					{:else}
-						<div class="flex items-center">
-							<input id="radio-1" type="radio" value="" name="default-radio" class="w-4 h-4 text-cyan-600 bg-gray-100 border-gray-300 dark:ring-offset-gray-800 focus:ring-0 dark:bg-gray-700 dark:border-gray-600">
-							<label for="radio-1" class="ml-2 max-w-min text-black dark:text-white">Bankkártya (min. 500Ft)</label>
-						</div>
+					<div class=" flex items-center justify-center bg-gray-200 dark:bg-slate-600 p-2 cursor-pointer rounded-lg" class:selectedpayment={paymentMethod == "Bankkártya"} on:click={() => {paymentMethod = 'Bankkártya'; console.log(paymentMethod);}}>
+						<span>Bankkártya</span>
+					</div>
 					{/if}
 				</div>
 			</div>
 
 			<div class="text-center">
 				<p class="text-center">Átvételi idő:</p>
-				<select class="w-full p-2 rounded-lg bg-neutral-700 focus:outline focus:ring-0 focus:outline-cyan-500">
-					<option class="w-full" selected>Válassz egy átvételi időpontot</option>
-					<option value="most">Most</option>
-					<option value="szunet1">1. szünet (8:45-8:55)</option>
-					<option value="szunet2">2. szünet (9:40-9:50)</option>
-					<option value="szunet3">3. szünet (10:35-10:45)</option>
-					<option value="szunet4">4. szünet (11:30-11:40)</option>
-					<option value="szunet5">Ebédszünet (12:25-12:45)</option>
-					<option value="szunet6">6. szünet (13:30-13:35)</option>
+				<select bind:value={atveteliIdo} class=" placeholder-black w-full p-2 rounded-lg dark:bg-neutral-700 bg-gray-200 focus:outline focus:ring-0 focus:outline-cyan-500">
+					<option class="w-full" disabled selected >Válassz egy átvételi időt</option>
+					{#each data.atveteliOpciok as opcio}
+						<option value={opcio.value}>{opcio.label}</option>
+					{/each}
 				</select>
 			</div>
 
 			<div class="flex flex-col w-full justify-center items-center mt-4 mb-3">
-				<button class=" bg-cyan-500 text-white dark:bg-neutral-600 p-2 rounded-xl w-10/12" on:click={() => confirmShow = true}>Rendelés leadása</button>
+				{#if bufeClosedStatus = 0}
+					<button class=" bg-cyan-500 text-white dark:bg-neutral-600 p-2 rounded-xl w-10/12" on:click={() => confirmShow = true}>Rendelés leadása</button>
+				{:else if bufeClosedStatus = 1}
+					<button disabled class=" bg-cyan-500 text-white dark:bg-neutral-600 p-2 rounded-xl w-10/12 opacity-60">Rendelés leadása</button>
+				{/if}
 			</div>
 		</div>
 		{/if}
 	</div>
 </main>
+<style>
+	.selectedpayment{
+		@apply bg-cyan-500 text-white border-none transition scale-105 ease-in-out;
+	}
+</style>
