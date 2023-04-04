b20f92baf76a6cc5ff4c8f747cceb7c6392c99ee
Saul Goodman <113334790+gergogyulai@users.noreply.github.com>
2023-04-04 02:08:18 +0200

    zaras api hajnali nyitas fix (csak talan jo, lusta vagyok testelni)

--- src/routes/api/zaras/+server.js
+++ src/routes/api/zaras/+server.js
@@ -18,19 +18,18 @@ function getMessage() {
   	const zaras = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 14, 0, 0);
   	const nyitas = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 7, 0, 0);
   
-  	if (isWeekend(dayOfTheWeek)) {
-    	bufeClosed.set(1);
-    	return "hetvege";
-  	} else if (zaras - now <= 0) {
-    	bufeClosed.set(1);
-    	return "closed";
-  	}
+	if (isWeekend(dayOfTheWeek)) {
+		bufeClosed.set(1);
+		return "hetvege";
+	} else if (now >= zaras) {
+		bufeClosed.set(1);
+		return "closed";
+	} else if (now < nyitas) {
+		bufeClosed.set(1);
+		return "closed";
+	}
   
   	const minutesUntil = getMinutesUntil(zaras);
-  
-  	if (minutesUntil <= 60) {
-    	bufeClosed.set(0);
-  	}
   
   	if (minutesUntil <= 10) {
   	  	return "10minLeft";
@@ -40,7 +39,7 @@ function getMessage() {
   	  	return "30minLeft";
   	} else if (minutesUntil <= 60) {
   	  	return "60minLeft";
-  	} else if (nyitas - now <= now) {
+  	} else if (now - nyitas <= 0) {
   	  	bufeClosed.set(0);
   	  	return "nyitva";
   	}
