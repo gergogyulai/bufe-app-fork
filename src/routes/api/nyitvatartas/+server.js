d9beac8c9b26cda6ba25e68b024afde1021cf460
Saul Goodman <113334790+gergogyulai@users.noreply.github.com>
2023-04-11 07:26:42 +0200

    fixed hetfo hajnali internal error

--- src/routes/api/nyitvatartas/+server.js
+++ src/routes/api/nyitvatartas/+server.js
@@ -1,68 +1,64 @@
-import { nyitasIdo, zarasIdo, bufeClosed } from "$lib/stores/Nyitvatartas.js";
+import singletonStore from '$lib/stores/Nyitvatartas.js';
 
 function isWeekend(dayOfWeek) {
-    return dayOfWeek === 0 || dayOfWeek === 6;
+  	return dayOfWeek === 0 || dayOfWeek === 6;
 }
 
 function getMinutesUntil(date) {
-    const now = new Date();
-    return Math.floor((date - now) / 60000);
+  	const now = new Date();
+  	return Math.floor((date - now) / 60000);
+}
+
+function getMessageForMinutesLeft(minutesUntil) {
+  	if (minutesUntil <= 10) {
+    	return "10minLeft";
+  	} else if (minutesUntil <= 20) {
+    	return "20minLeft";
+  	} else if (minutesUntil <= 30) {
+    	return "30minLeft";
+  	} else if (minutesUntil <= 60) {
+    	return "60minLeft";
+  	} else {
+    	return "nyitva";
+  	}
 }
 
 function getMessage() {
-    const now = new Date();
-    const dayOfTheWeek = now.getDay();
+  	const now = new Date();
+  	const dayOfTheWeek = now.getDay();
 
-    let nyitasValue = {hour: 7, minute: 0};
-    let zarasValue = {hour: 14, minute: 0};
-	
-    nyitasIdo.subscribe(value => {
-        nyitasValue = value;
-        console.log(value);
-    });
-    zarasIdo.subscribe(value => {
-        zarasValue = value;
-        console.log(value);
-    });
+  	let storeState;
+  	singletonStore.subscribe(value => {
+  	  	storeState = value;
+  	});
 
-    const nyitas = new Date(now.getFullYear(), now.getMonth(), now.getDate(), nyitasValue.hour, nyitasValue.minute, 0);
-    const zaras = new Date(now.getFullYear(), now.getMonth(), now.getDate(), zarasValue.hour, zarasValue.minute, 0);
+  	const nyitasValue = storeState.nyitasIdo[0];
+  	const zarasValue = storeState.zarasIdo[0];
 
-    if (isWeekend(dayOfTheWeek)) {
-        bufeClosed.set(true)
-        return "hetvege";
-    } else if (now >= zaras) {
-        bufeClosed.set(true)
-        return "closed";
-    } else if (now < nyitas) {
-        bufeClosed.set(true)
-        return "closed";
-    }
+  	const nyitas = new Date(now.getFullYear(), now.getMonth(), now.getDate(), nyitasValue.hours, nyitasValue.minutes, 0);
+  	const zaras = new Date(now.getFullYear(), now.getMonth(), now.getDate(), zarasValue.hours, zarasValue.minutes, 0);
 
-    const minutesUntil = getMinutesUntil(zaras);
+  	console.log("nyitas object:", nyitas);
+  	console.log("zaras object:", zaras);
 
-    if (minutesUntil <= 10) {
-        return "10minLeft";
-    } else if (minutesUntil <= 20) {
-        return "20minLeft";
-    } else if (minutesUntil <= 30) {
-        return "30minLeft";
-    } else if (minutesUntil <= 60) {
-        return "60minLeft";
-    } else if (now - nyitas <= 0) {
-        bufeClosed.set(false)
-        return "nyitva";
-    } else {
-        return "asdasasdasd";
-    }
+  	if (isWeekend(dayOfTheWeek)) {
+  	  	return "hetvege";
+  	} else if (now < nyitas) {
+		return "notYetOpened";
+	} else if (zaras - now <= 0) {
+  	  	return "closed";
+  	}
+
+  	const minutesUntil = getMinutesUntil(zaras);
+  	return getMessageForMinutesLeft(minutesUntil);
 }
 
 export function GET() {
-    const message = getMessage();
-
-    const body = JSON.stringify({ message });
-    const headers = { "Content-Type": "application/json" };
-    const response = new Response(body, { headers });
-
-    return response;
+  const message = getMessage();
+  
+  const body = JSON.stringify({ message });
+  const headers = { "Content-Type": "application/json" };
+  const response = new Response(body, { headers });
+  
+  return response;
 }
